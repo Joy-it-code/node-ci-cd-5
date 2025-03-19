7e036f2903fc8e12c1586dcc020df0459b388fdc
Joy-it-code <joynwat2014@gmail.com>
2025-03-19 04:51:07 +0000

    workflow file

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,5 +1,6 @@
 module.exports = {
   languageOptions: {
+    ecmaVersion: "latest", // Specify ECMAScript version
     globals: {
       window: "readonly", // For browser
       document: "readonly", // For browser
@@ -8,6 +9,8 @@ module.exports = {
     },
   },
   rules: {
-    "no-console": "off",
+    "no-console": "off", // Allow console statements
+    "semi": ["error", "always"], // Enforce semicolons
   },
+  ignores: [".env", "node_modules/"], // Files or directories to ignore
 };
